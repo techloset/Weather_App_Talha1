357f14a44300d9fe334e5741ff620d468586f9c8
talhaunas1 <mtalha4600@gmail.com>
2023-12-15 12:55:27 +0500

    fetching weather data

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,54 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react'
-import axios from 'axios'
+import React, { useState } from "react";
+import axios from "axios";
+import Image from "next/image";
+
 
 const page = () => {
-const [city, setCity] = useState('')
-const [weather, setWeather] = useState({})
-const [loader, setLoader] = useState(false)
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({});
+  const [loader, setLoader] = useState(false);
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=dubai&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
-  
-const fetchWeather=(e)=>{
-  e.preventDefault()
-  setLoader(true)
+  // console.log(city);
 
-  axios.get(url).then((response) => {
-    setWeather(response.data)
-    console.log("response.data" ,response.data);
-  })
-  setCity('')
-  setLoader(false)
-}
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city }&units=metric&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
 
-return (
+  const fetchWeather = (e) => {
+    e.preventDefault();
+    setLoader(true);
+
+    axios.get(url).then((response) => {
+      setWeather(response.data);
+      console.log( response.data);
+    });
+    setCity("");
+    setLoader(false);
+  };
+
+  return (
     <div>
       <h1>Weather App</h1>
-      <button onClick={fetchWeather} className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'>Fetch Weather</button>
-    </div>
-  )
-}
 
-export default page
+      <input
+        type="text"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Search City"
+        required
+        onChange={(e)=> setCity(e.target.value)}
+      />
+      <button
+        onClick={fetchWeather}
+        className="bg-white mt-2 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+      >
+        Fetch Weather
+      </button>
+
+      {weather.weather && (
+        <p>{weather.weather[0].main}</p>
+      )}
+    </div>
+  );
+};
+
+export default page;
